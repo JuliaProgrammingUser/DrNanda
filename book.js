aba76f3313186c32bd553a1fdf44bdcee96943bd
JuliaProgrammingUser <151858405+JuliaProgrammingUser@users.noreply.github.com>
2023-12-20 22:57:10 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,46 @@
+
+
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".header .navbar");
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".team-slider",{
+    loop:true,
+    grabCursor:true,
+    spaceBetween:20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
